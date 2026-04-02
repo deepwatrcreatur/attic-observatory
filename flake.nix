324822b77f4afbc379abcc3a0dada8887f6275ea
Anwer Khan <deepwatrcreatur@gmail.com>
2026-04-01 23:54:00 -0400

    ci: add flake checks and app output

--- flake.nix
+++ flake.nix
@@ -1,29 +1,27 @@
 {
   description = "attic-observatory: lightweight Attic cache dashboard";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      systems = [
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem
+      [
         "x86_64-linux"
         "aarch64-linux"
-      ];
-      forAllSystems =
-        f:
-        nixpkgs.lib.genAttrs systems (
-          system:
-          f {
-            pkgs = import nixpkgs { inherit system; };
-          }
-        );
-    in
-    {
-      packages = forAllSystems (
-        { pkgs }:
-        {
-          default = pkgs.stdenvNoCC.mkDerivation {
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          attic-observatory = pkgs.stdenvNoCC.mkDerivation {
             pname = "attic-observatory";
             version = "0.1.0";
             src = ./.;
@@ -41,7 +39,32 @@
               runHook postInstall
             '';
           };
+        in
+        {
+          packages.default = attic-observatory;
+
+          apps.default = {
+            type = "app";
+            program = "${attic-observatory}/bin/attic-observatory";
+            meta.description = "Run the attic-observatory dashboard";
+          };
+
+          checks.unit-tests =
+            pkgs.runCommand "attic-observatory-unit-tests" { nativeBuildInputs = [ pkgs.python3 ]; }
+              ''
+                cd ${self}
+                python3 -m unittest -v test_app.py
+                touch $out
+              '';
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.python3
+              pkgs.sqlite
+            ];
+          };
+
+          formatter = pkgs.nixfmt-rfc-style;
         }
       );
-    };
 }
